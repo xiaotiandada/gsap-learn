35403a80876ed7763b0f9bb100386e86ee132e45
xiaotian <istianlei@qq.com>
2023-03-20 18:38:39 +0800

    feat: scrollTrigger

---
+++ README.md
@@ -0,0 +1,12 @@
+# Vite + React
+
+This is a [Vite](https://vitejs.dev) project together with React.
+
+[![Edit in CodeSandbox](https://assets.codesandbox.io/github/button-edit-lime.svg)](https://codesandbox.io/p/github/codesandbox/codesandbox-template-vite-react/main)
+
+[Configuration](https://codesandbox.io/docs/projects/learn/setting-up/tasks) has been added to optimize it for [CodeSandbox Projects](https://codesandbox.io/p/dashboard).
+
+## Resources
+
+- [Vite — GitHub](https://github.com/vitejs/vite)
+- [Vite — Docs](https://vitejs.dev/guide/)
--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,52 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useRef, useEffect } from 'react'
+import gsap from 'gsap'
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
 import './App.css'
+gsap.registerPlugin(ScrollTrigger);
 
 function App() {
-  const [count, setCount] = useState(0)
+  const circleRef = useRef(null);
+  useEffect(() => {
+    gsap.fromTo("#hero", {
+      opacity: 0.9,
+      scale: 0.2,
+      y: '-50vh'
+    },
+      {
+        opacity: 1,
+        y: 0,
+        scale: 1,
+        duration: 1,
+        ease: "none",
+        scrollTrigger: {
+          trigger: '#first',
+          start: "top center",
+          end: "bottom top",
+          scrub: true
+        }
+      });
+  }, []);
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div id='first' className='h-[100vh]'>
+        <h1 className="text-3xl font-bold underline text-center pt-[40vh]">
+          Bring it all together
+        </h1>
+        <p className='text-center mt-4'>The fastest way to combine your favorite tools and APIs to build the fastest sites, stores, and apps for the web.</p>
+        <div className='text-center mt-10'>
+          <button>Get started for free</button>
+          <button>Request demo</button>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
+      <section id='second' className=''>
+        <img id='hero' className='w-[690px] mx-auto' alt="" loading="lazy" src="https://d33wubrfki0l68.cloudfront.net/86e04f2182b562bb20646414d9e6fb13dc9f824a/bea27/v3/img/build/8t9i3uomzs-1280.png" width="1280" height="800" />
+      </section>
+
     </div>
-  )
+  );
 }
 
 export default App
